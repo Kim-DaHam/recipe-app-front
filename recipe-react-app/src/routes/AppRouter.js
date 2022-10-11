5b41b3b410015cfc54ea1b368c3b8ffdfdd6d922
Kim-DaHam <times7749@naver.com>
2022-10-11 10:52:33 +0900

    write AppRouter.js

---
+++ recipe-react-app/src/routes/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import App from '../App';
+import Home from '../Home';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+import AdminPage from './AdminPage/AdminPage';
+
+class AppRouter extends React.Component {
+    render(){
+        return(
+            <BrowserRouter>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<App/>}/>
+                        <Route path="/home" element={<Home/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="/admin-page" element={<AdminPage/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default AppRouter;
