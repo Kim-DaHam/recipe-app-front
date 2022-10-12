b27dd1bc1098192115bda5d818f2cce4f29965db
Kim-DaHam <times7749@naver.com>
2022-10-12 17:46:42 +0900

    회원가입 검증 중

--- recipe-react-app/src/routes/Signup/Signup.js
+++ recipe-react-app/src/routes/Signup/Signup.js
@@ -11,42 +11,46 @@ class Signup extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         let form = document.getElementById('signup-form');
-        let input_uname = document.getElementById('uname');
-        let input_uemail = document.getElementById('uemail');
-        let input_upw = document.getElementById("upw");
-        let input_upwh = document.getElementById("upwh");
+        let input_name = document.getElementById('name-input');
+        let input_email = document.getElementById('email-input');
+        let input_pwd = document.getElementById("pwd-input");
+        let input_pwdh = document.getElementById("pwdh-input");
 
         const data = new FormData(event.target);
         const name = data.get('uname');
         const email = data.get('uemail');
-        const pw = data.get('upw');
-        const pwh = data.get('upwh');
+        const pwd = data.get('upwd');
+        const pwdh = data.get('upwdh');
 
         console.log('회원가입 버튼 클릭!');
-        //console.log(name.length);
+        //console.log(input_email.childNodes);
 
         if(name.length == 0){
             console.log('이름 미입력!');
-            input_uname.autofocus = true;
+            input_name.childNodes[0].focus();
             form.classList.add('was-validated');
+            return;
         }
-        if(name.length == 0){
+        if(email.length == 0){
             console.log('이메일 미입력!');
-            input_uemail.autofocus = true;
+            input_email.childNodes[0].focus();
             form.classList.add('was-validated');
+            return;
         }
-        if(name.length == 0){
+        if(pwd.length == 0){
             console.log('비밀번호 미입력!');
-            input_upw.autofocus = true;
+            input_pwd.childNodes[0].focus();
             form.classList.add('was-validated');
+            return;
         }
-        if(pw != pwh){
+        if(pwd != pwdh){
             console.log('비밀번호 확인 불일치!');
-            input_upwh.autofocus = true;
+            input_pwdh.childNodes[0].focus();
             form.classList.add('was-validated');
+            return;
         }
 
-        signup({mname: name, memail: email, mpw: pw})
+        signup({mname: name, memail: email, mpw: pwd})
         .then((response) => {
             window.location.href="/login";
         })
@@ -61,12 +65,13 @@ class Signup extends React.Component {
                         <div className="w3-center">
                             <form id="signup-form" onSubmit={this.handleSubmit} action="/action_page.php" className="needs-validation" noValidate>
                                 <label>이름</label>
-                                <div className="form-row">
+                                <div id="name-input" className="form-row">
                                     <input type="text" className="form-control" 
+                                    maxLength='25'
                                     id="uname" placeholder="이름을 입력하세요." 
                                     name="uname" required/>
                                     <div className="valid-feedback"></div>
-                                    <div className="invalid-feedback">-</div>
+                                    <div className="invalid-feedback">이름을 입력하세요.</div>
                                 </div>
                                 <label>이메일</label>
                                 <div id="email-input" className="form-row">
@@ -74,22 +79,24 @@ class Signup extends React.Component {
                                     id="uemail" placeholder="이메일을 입력하세요." 
                                     name="uemail" required/>
                                     <div className="valid-feedback"></div>
-                                    <div className="invalid-feedback">-</div>
+                                    <div className="invalid-feedback">이메일을 입력하세요.</div>
                                     <button id="dupl-btn" type="button" class="btn btn-danger">중복확인</button>
                                 </div>
                                 <label>비밀번호</label>
-                                <div className="form-row">
+                                <div id="pwd-input" className="form-row">
                                     <input type="text" className="form-control" 
-                                    id="upw" placeholder="비밀번호를 입력하세요." 
-                                    name="upw" required/>
+                                    maxLength='15'
+                                    id="upwd" placeholder="비밀번호를 입력하세요." 
+                                    name="upwd" required/>
                                     <div className="valid-feedback"></div>
-                                    <div className="invalid-feedback">-</div>
+                                    <div className="invalid-feedback">비밀번호를 입력하세요.</div>
                                 </div>
                                 <label>비밀번호 확인</label>
-                                <div className="form-row">
+                                <div id="pwdh-input" className="form-row">
                                     <input type="text" className="form-control" 
-                                    id="upwh" placeholder="비밀번호를 입력하세요." 
-                                    name="upwh" required/>
+                                    maxLength='15'
+                                    id="upwdh" placeholder="비밀번호를 입력하세요." 
+                                    name="upwdh" required/>
                                     <div className="valid-feedback"></div>
                                     <div className="invalid-feedback">비밀번호가 일치하지 않습니다.</div>
                                 </div>
