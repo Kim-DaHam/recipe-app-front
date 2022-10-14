57305436b1397cf078bbda59e2f3ff431cc19cca
Kim-DaHam <times7749@naver.com>
2022-10-14 12:35:48 +0900

    Fridge - Fridge Container

--- recipe-react-app/src/routes/Fridge/Fridge.js
+++ recipe-react-app/src/routes/Fridge/Fridge.js
@@ -21,8 +21,8 @@ class Fridge extends React.Component {
                         {/* Fridge */}
                         <div id="fridge-container" class="w3-card-4 w3-margin w3-white">
                             {/* Ingredients SearchBar */}
-                            <div class="w3-container">
-                                <form id="ingredients-searchBar" class="form-inline" action="/action_page.php">
+                            <div id="ingredients-searchBar" class="w3-container">
+                                <form class="form-inline" action="/action_page.php">
                                     <div class="input-group">
                                         <input type="text" class="form-control" placeholder="식재료  검색"/>
                                         <div class="input-group-prepend">
@@ -30,20 +30,38 @@ class Fridge extends React.Component {
                                         </div>
                                     </div>
                                 </form>
+                                <select name="i-category" id="i-category">
+                                    <option value="">--선택안함--</option>
+                                    <option value="채소">채소</option>
+                                    <option value="과일">과일</option>
+                                    <option value="유제품">유제품</option>
+                                    <option value="유제품">육류</option>
+                                    <option value="기타">기타</option>
+                                </select>
                             </div>
                             {/* Ingredients List */}
-                            <div class="w3-container">
-                            <div class="list-group">
-                                <a href="#" class="list-group-item list-group-item-action">First item</a>
-                                <a href="#" class="list-group-item list-group-item-action">Second item</a>
-                                <a href="#" class="list-group-item list-group-item-action">Third item</a>
+                            <div id="ingredients-list" class="w3-container">
+                                <div class="list-group">
+                                    <a href="#" class="list-group-item list-group-item-action">
+                                        <input type="checkbox" class="form-check-input"/>
+                                        First item
+                                        <button type="button" class="btn btn-danger w3-right"><b>―</b></button>
+                                    </a>
                                 </div>
-                                <div class="w3-row">
+                                <div id="delete-btn" class="w3-row">
                                     <div class="w3-col m8 s12">
-                                    <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                                        <p>
+                                            <button class="w3-button w3-padding-small w3-white w3-border">
+                                                <b>전체삭제</b>
+                                            </button>
+                                        </p>
                                     </div>
                                     <div class="w3-col m4 w3-hide-small">
-                                    <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+                                        <p>
+                                            <span class="w3-padding-small w3-right">
+                                                <span class="w3-tag">0</span><b>  선택삭제</b>
+                                            </span>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
